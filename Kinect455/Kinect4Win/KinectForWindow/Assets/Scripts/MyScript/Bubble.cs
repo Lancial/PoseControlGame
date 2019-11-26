081b343598a8480825171f194cdf5902b709937e
Nguyen Duong <nguyend2@uw.edu>
2019-11-26 14:23:05 -0800

    add unity project

---
+++ Kinect455/Kinect4Win/KinectForWindow/Assets/Scripts/MyScript/Bubble.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    private Vector3 mMovementDirection = Vector3.zero;
+    private Coroutine mCurrentChanger = null;
+
+    private void OnEnable()
+    {
+        mCurrentChanger = StartCoroutine(DirectionChanger());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(mCurrentChanger);
+    }
+
+    private void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += mMovementDirection * Time.deltaTime * 0.5f;   
+    }
+
+    private IEnumerator DirectionChanger()
+    {
+        while(gameObject.activeSelf)
+        {
+            mMovementDirection = new Vector2(Random.Range(0, 100) * 0.01f, Random.Range(0, 100) * 0.01f);
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+}
