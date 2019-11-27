1f7d60f080407de9652a760a6cf1fe02ab4ac28b
xiaoliang <lx1030@uw.edu>
2019-11-26 19:24:09 -0800

    mnist nn works

--- Classifier/net.py
+++ Classifier/net.py
@@ -7,14 +7,14 @@ class Net(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.fc_layer1 = nn.Linear(75, 48)
-        self.fc_layer2 = nn.Linear(48, 48)
-        self.fc_layer3 = nn.Linear(48, 48)
-        self.fc_layer4 = nn.Linear(48, 6)
+        self.fc_layer1 = nn.Linear(28*28, 64)
+        self.fc_layer2 = nn.Linear(64, 64)
+        self.fc_layer3 = nn.Linear(64, 64)
+        self.fc_layer4 = nn.Linear(64, 10)
 
     def forward(self, x):
-        x = F.leaky_relu(self.fc_layer1(x))
-        x = F.leaky_relu(self.fc_layer2(x))
-        x = F.leaky_relu(self.fc_layer3(x))
+        x = F.relu(self.fc_layer1(x))
+        x = F.relu(self.fc_layer2(x))
+        x = F.relu(self.fc_layer3(x))
         x = self.fc_layer4(x)
         return F.log_softmax(x, dim=1)
