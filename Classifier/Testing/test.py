e9a9e516fd6a359b00e764bdddb1ec33767bfb6f
xiaoliang <lx1030@uw.edu>
2019-12-01 23:47:38 -0800

    testing

--- Classifier/Testing/test.py
+++ Classifier/Testing/test.py
@@ -7,15 +7,13 @@ import torch.optim as optim
 from os import path
 from SimpleDataSet import SimpleDataSet
 
-class ToTensor(object):
-    def __call__(self, sample):
-        pose, joint_set = sample['pose_label'], sample['skeleton_data']
-        return {'pose_label': torch.Tensor(pose), 'skeleton_data': torch.Tensor(joint_set)}
-
 csv_directory = path.dirname(path.realpath(__file__))
 csv_path = path.join(csv_directory, 'skeleton_experiment.csv')
 
-train = SimpleDataSet(csv_file=csv_path, root_dir=csv_path, transform=transforms.Compose([ToTensor()]))
+# train = SimpleDataSet(csv_file=csv_path, root_dir=csv_path,
+#                       transform=transforms.Compose([transforms.ToTensor()]))
+train = SimpleDataSet(csv_file=csv_path, root_dir=csv_path,
+                      transform=None)
 trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
 
 
