a2d72f08d806974a472b7c15650607b6a354d1e1
Nguyen Duong <ducnguyenduchao@gmail.com>
2019-12-07 20:15:41 -0800

    Fix game movement

---
+++ Kinect455/Kinect4Win/KinectForWindow/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smooth = 10f;
+    public Vector3 offset;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smooth * Time.deltaTime);
+        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
+
+    }
+}
